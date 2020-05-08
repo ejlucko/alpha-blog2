9a48db87541e0448b76c5d5e6c8a376f59c2e9b2
Eric Lucko <eric.lucko@arcadiasolutions.com>
2020-05-07 20:07:10 -0400

    adds update article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,39 @@
 class ArticlesController < ApplicationController
 
-def index
-  @articles = Article.all
-end
+  def index
+    @articles = Article.all
+  end
 
-def show
-  @article = Article.find(params[:id])
-end
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description)) #whitelisting strong parameters Article.new(params[:article]) won't work.
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article #shorthand for`redirect_to article_path(@article) which will grab the article id and match the routes show path for articles
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
 end
